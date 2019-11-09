183e88e6460b8403013319e1c4d1374a7baca557
Kevin Marinas <marinaskevin@outlook.com>
2019-11-08 22:30:43 -0800

    Debugged a p5.js program so that the new 'Ball' objects generated by mouse clicks can fall and bounce as intended

---
+++ p5.js/Drag-Drop-Debug/sketch.js
@@ -0,0 +1,33 @@
+var gravityAcceleration = 1;
+var ballArray = [];
+function setup() {
+    createCanvas(720, 480);
+}
+function draw() {
+    background(25);
+    for(var i = 0; i< ballArray.length; i++){
+        ballArray[i].move();
+        ballArray[i].display();
+    }
+}
+function mouseReleased(){
+    ballArray.push(new Ball(width/2, 0));
+}
+function Ball(xPosition, yPosition) {
+    this.x = xPosition;
+    this.y = yPosition;
+    this.diameter = 50;
+    this.speed = 0;
+    this.move = function () {
+        this.y += this.speed;
+        if (this.y >= (height - this.diameter / 2)) {
+            this.y = height - this.diameter / 2;
+            this.speed = -0.9 * this.speed;
+        } else {
+            this.speed += gravityAcceleration;
+        }
+    }
+    this.display = function () {
+        ellipse(this.x, this.y, this.diameter, this.diameter);
+    }
+}
